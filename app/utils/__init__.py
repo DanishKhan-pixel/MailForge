c79d56301d2c33e2980b0128b768fcd99f35c2dc
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-09-17 19:34:00 +0500

    refactor(utils): export chunk_list in utils package init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from app.utils.batch_utils import chunk_list
 from app.utils.datetime_utils import format_utc_timestamp, parse_iso_timestamp
 from app.utils.formatters import mask_email
 from app.utils.string_utils import truncate_text
@@ -11,4 +12,5 @@ __all__ = [
     "format_utc_timestamp",
     "parse_iso_timestamp",
     "truncate_text",
+    "chunk_list",
 ]
