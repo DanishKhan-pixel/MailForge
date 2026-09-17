4d0fbe89735f8166bfc8fbc08efc981d5327b971
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-09-18 02:27:11 +0500

    refactor(utils): export extract_email_domain in utils package init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from app.utils.batch_utils import chunk_list
 from app.utils.datetime_utils import format_utc_timestamp, parse_iso_timestamp
+from app.utils.email_utils import extract_email_domain
 from app.utils.formatters import mask_email
 from app.utils.string_utils import truncate_text
 
@@ -13,4 +14,5 @@ __all__ = [
     "parse_iso_timestamp",
     "truncate_text",
     "chunk_list",
+    "extract_email_domain",
 ]
