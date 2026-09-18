bb121a855ad7e05b2e28f9a99757b6ee3bbb9ae0
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-09-18 19:21:32 +0500

    refactor(utils): export sanitize_subject in utils package init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -6,7 +6,7 @@ from app.utils.batch_utils import chunk_list
 from app.utils.datetime_utils import format_utc_timestamp, parse_iso_timestamp
 from app.utils.email_utils import extract_email_domain
 from app.utils.formatters import mask_email
-from app.utils.string_utils import truncate_text
+from app.utils.string_utils import sanitize_subject, truncate_text
 
 __all__ = [
     "mask_email",
@@ -15,4 +15,5 @@ __all__ = [
     "truncate_text",
     "chunk_list",
     "extract_email_domain",
+    "sanitize_subject",
 ]
